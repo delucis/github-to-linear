38735bb34195f1a7160db88ce24fb1dfe43ac6dc
Chris Swithinbank <swithinbank@gmail.com>
2023-04-03 14:06:22 +0200

    Fix built extension manifest for Chromium compatibility (#10)

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -1,15 +1,26 @@
 import fs from 'node:fs/promises';
 import pkg from '../package.json' assert { type: 'json' };
+import manifest from '../extension/manifest.json' assert { type: 'json' };
 
 console.log('Copying source files to dist...');
 await fs.cp('extension', 'dist', { recursive: true });
 
 console.log('Updating version number in manifest.json...');
-let manifest = await fs.readFile('dist/manifest.json', 'utf-8');
-manifest = manifest.replace(
-  '"version": "0.0.0"',
-  `"version": "${pkg.version}"`
+manifest.version = pkg.version;
+
+if (process.argv.includes('--chromium')) {
+  console.log('Updating manifest.json for Chromium compatibility...');
+  manifest.manifest_version = 3;
+  const swSource = manifest.background.scripts[0];
+  manifest.background.service_worker = swSource;
+  delete manifest.background.scripts;
+  delete manifest.options_ui.chrome_style;
+}
+
+await fs.writeFile(
+  'dist/manifest.json',
+  JSON.stringify(manifest, null, 2),
+  'utf-8'
 );
-await fs.writeFile('dist/manifest.json', manifest, 'utf-8');
 
 console.log('Done!');
